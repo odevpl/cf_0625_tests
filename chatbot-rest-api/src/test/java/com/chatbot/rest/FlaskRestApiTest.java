cf2f49d140f884d57b9804a7677b4eedd16f79ff
Aga Gorny <aga.gorny33@gmail.com>
2025-08-13 17:13:48 +0200

    response test deleted

--- chatbot-rest-api/src/test/java/com/chatbot/rest/FlaskRestApiTest.java
+++ chatbot-rest-api/src/test/java/com/chatbot/rest/FlaskRestApiTest.java
@@ -1,13 +1,11 @@
 package com.chatbot.rest;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
- class FlaskRestApiTest {
+class FlaskRestApiTest {
 
     @Test
     void getApiTest() {
@@ -18,29 +16,5 @@ import static io.restassured.RestAssured.given;
                 .when()
                 .get("/")
                 .then().statusCode(200);
-    }
-
-    @Test
-    void testGetResponse() {
-        RestAssured.baseURI = "http://localhost:5000";
-
-        Response response = given()
-                .when()
-                .get("/").then().extract().response();
-
-        String responseBody = response.prettyPrint();
-        String expected = "<html>\n" +
-                "  <body>\n" +
-                "\n" +
-                "    Hello, World!    <br clear=\"none\"/>\n" +
-                "\n" +
-                "    Debug=False    <br clear=\"none\"/>\n" +
-                "\n" +
-                "    DB=sqlite:///D:\\ProjectsPython\\cf_0625\\config\\chatbot.db\n" +
-                "      </body>\n" +
-                "</html>";
-
-        Assertions.assertEquals(expected, responseBody);
-
     }
 }
